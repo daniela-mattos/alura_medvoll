2612ff064aad5bdc44366419230171e208c3257c
daniela-mattos <kittybhank@hotmail.com>
2024-06-06 18:50:25 -0300

    iniciando filtro

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -1,4 +1,38 @@
 package med.voll.api.infra.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String gerarToken(Usuario usuario) {
+
+        try {
+            var algorimo = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withExpiresAt(expiraEm())
+                    .sign(algorimo);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao gerar token jwt", e);
+        }
+    }
+    private Instant expiraEm() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
+
+
